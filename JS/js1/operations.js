9306a06294af3c71796c4a77af15e79447315b6c
Shaldonbarnes10 <shaldonbarnes05@gmail.com>
2025-06-24 11:58:23 +0530

    Comitted on jun 24 2025

---
+++ JS/js1/operations.js
@@ -0,0 +1,12 @@
+let addition = 4+5
+let subb = 5-1
+let mul = 4*5
+let divi = 8/2
+let remainder = 4%5
+let expo= 2**3
+
+let myscore = 220;
+myscore++;
+
+let credits = 10;
+credits--;
---
+++ JS/js1/printing.js
@@ -0,0 +1,8 @@
+console.table({city : "Dubai"})
+let variable_for_test = 25
+variable_for_test++;
+let var2 = variable_for_test;
+
+console.log(variable_for_test);
+console.log(var2);
+console.log("Hello")
